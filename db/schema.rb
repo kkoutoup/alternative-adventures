707fb7bf2a46ffb7665eeaaf28b9837166e9c7dc
Vishal Patel <vishal.patel@sidewayseight.co.uk>
2020-08-01 14:17:50 +0100

    added bookings model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_100849) do
+ActiveRecord::Schema.define(version: 2020_08_01_125725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "experience_id", null: false
+    t.boolean "booked"
+    t.date "starts_at"
+    t.date "ends_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["experience_id"], name: "index_bookings_on_experience_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "experiences", force: :cascade do |t|
     t.string "title"
@@ -40,5 +52,7 @@ ActiveRecord::Schema.define(version: 2020_08_01_100849) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "experiences"
+  add_foreign_key "bookings", "users"
   add_foreign_key "experiences", "users"
 end
