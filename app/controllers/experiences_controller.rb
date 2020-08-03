e3059df0e738905afffb23b489e4886040e1016a
Enrica Fedeli-Jaques <enricafedeli@gmail.com>
2020-08-03 15:13:18 +0200

    completed the remaining crud action in controller . created show page and new page

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,4 +1,5 @@
 class ExperiencesController < ApplicationController
+  before_action :find_experience, only: [:show, :edit, :destroy]
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
@@ -9,17 +10,43 @@ class ExperiencesController < ApplicationController
   end
 
   def new
+    @experience = Experience.new
   end
 
   def create
+    @experience = Experience.new(strong_params)
+    @experience.user = current_user
+    if @experience.save
+      redirect_to experience_path(@experience)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @experience.update(strong_params)
+    if @experience.update
+      redirect_to experience_path(@experience)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @experience.destroy
+    redirect_to experiences_path
+  end
+
+  private
+
+  def strong_params
+    params.require(:experience).permit(:title, :experience_type, :description, :price)
+  end
+
+  def find_experience
+    @experience = Experience.find(params[:id])
   end
 end
