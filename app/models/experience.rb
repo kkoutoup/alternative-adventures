877cbb31b033071d3341cce57f746c2ebd8c3797
Vishal Patel <vishal.patel@sidewayseight.co.uk>
2020-08-03 17:27:56 +0100

    Added the edit and destroy functionality and tested views for experiences

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,7 +1,7 @@
 class Experience < ApplicationRecord
   belongs_to :user
   has_many :reviews, through: :bookings
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :title, :experience_type, :description, presence: true
   validates :title, length: { minimum: 10 }
   validates :description, length: { in: 15..500 }
